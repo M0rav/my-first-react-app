0d88c5983687a3fe648a0ae3b44e0d53a9e90ea2
Petrik Diak <diak@petrik.hu>
2023-01-10 13:03:28 +0100

    final

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
+import React, { ChangeEvent, Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface State {
+  datum: Date;
+  dobas: number;
+  meret: number;
 }
+class App extends Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      datum: new Date(),
+      dobas: 0,
+      meret: 14,
+    }
+    setInterval(() => {
+      this.setState({
+        datum: new Date()
+      });
+    }, 1000);
+  }
+  kockadobas =() => {
+    const ujSzam = Math.floor(Math.random() * 6 + 1 );//0-6 között ad random számot
+    this.setState({
+      dobas:ujSzam
+    })
+  }
+  betumeret =(e: ChangeEvent) => {
+    let ujmeret = parseInt((e.currentTarget as HTMLInputElement).value);
+    this.setState({
+      meret: ujmeret
+    })
+  }
 
+  render() {
+    return <div style={{ fontSize: this.state.meret + "pt"}}>
+      <p id="hello" style ={{
+        fontStyle: 'italic',
+        color: 'limegreen'   
+      }}>
+        { this.state.datum.toLocaleString() }
+      </p>
+      <p className="kockadobas">Kockadobás eredménye: {this.state.dobas }</p>
+      <button onClick={this.kockadobas}>Kockadobás</button>
+      <div>
+    <label htmlFor="FontSizeChange">Kérlek add meg a kívánt betűméretet: </label>
+    <input type="number" placeholder='Ide' min={8} onChange={this.betumeret}/>
+    </div>
+    </div>;
+   
+  }
+}
 export default App;
